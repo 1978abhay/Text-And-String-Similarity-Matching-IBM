67de6d1fe86db58367109a81f65caef3d5e0dd77
Nehal <batran@tcd.ie>
2023-02-14 11:53:18 +0000

    code to run button and textbox template

--- frontpage.py
+++ frontpage.py
@@ -1,11 +1,10 @@
-from flask import Flask #, render_template
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    #return render_template("practice.html")
-    return "Hello World!"
+def homepage():
+    return render_template("practice.html")
 
-#if __name__ == "__main__":
-   # app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug = True)
