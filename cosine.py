df5e87a8b305ace580408e33e473707c2e003726
Joe <handjo@tcd.ie>
2023-03-02 12:52:35 +0000

    Initial Integration
    Currently takes in input, and will perform similarity matching on the given values, only using cosine similarity so far

--- cosine.py
+++ cosine.py
@@ -26,11 +26,11 @@ def text_to_vector(text):                                         # Vector of th
 
 
 # Will change to import csv
-X = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
-Y = "Bees are flying insects closely related to wasps and ants, known for their role in pollination and, in the case of the best-known bee species, the western honey bee, for producinghoney. Bees are a monophyletic lineage within the superfamily Apoidea. They are presently considered a clade, called Anthophila. There are over 16,000 known species of bees in seven recognized biological families. Some species – including honey bees, bumblebees, and stingless bees – live socially in colonies while some species – including mason bees, carpenter bees, leafcutter bees, and sweat bees – are solitary."
+#X = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
+#Y = "Bees are flying insects closely related to wasps and ants, known for their role in pollination and, in the case of the best-known bee species, the western honey bee, for producinghoney. Bees are a monophyletic lineage within the superfamily Apoidea. They are presently considered a clade, called Anthophila. There are over 16,000 known species of bees in seven recognized biological families. Some species – including honey bees, bumblebees, and stingless bees – live socially in colonies while some species – including mason bees, carpenter bees, leafcutter bees, and sweat bees – are solitary."
 
-vector1 = text_to_vector(X)
-vector2 = text_to_vector(Y)
+#vector1 = text_to_vector(X)
+#vector2 = text_to_vector(Y)
 
-cosine = get_cosine(vector1, vector2)
-print("Cosine similarity score is:", round(cosine,3))
+#cosine = get_cosine(vector1, vector2)
+#print("Cosine similarity score is:", round(cosine,3))
--- frontpage.py
+++ frontpage.py
@@ -1,37 +1,65 @@
-from flask import Flask , render_template, redirect, url_for, request
+import requests
+from flask import Flask, session, render_template, redirect, url_for, request, jsonify
+import json
+import cosine
 
 app = Flask(__name__)
 
+compresult = ""
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
+
 
 @app.route('/compare')
 def compare():
     return render_template('score.html')
 
+
 @app.route('/about')
 def about():
     return render_template('infopage.html')
+
 
 @app.route('/rerouter')
 def rerouter():
     return redirect('/')
 
 
+@app.route('/testFunc/<string:string1>/<string:string2>')
+def tryprocess(string1, string2):
+    string1Vect = cosine.text_to_vector(string1)
+    string2Vect = cosine.text_to_vector(string2)
+    compresult = cosine.get_cosine(string1Vect, string2Vect)
 
-#@app.route("/compare", methods=["POST", "GET"])
-#def compare():
-   # if request.method == "POST":
-    #    simscore = request.form["xy"]
-    #    return redirect(url_for("score", simscore=score))
-    #else:
-    #    return render_template("compare.html")
-#@app.route("/score")
-#def score():
-#def score (simscore):
-   # return f"<h1>{simscore}</h1>"
-   # return "hello"
+    print("string1 : ", string1, "string2 : ", string2)
+    print("result is ", compresult)
+
+
+    return str(compresult)
+
+@app.route('/compResult')
+def fetchResult():
+    print(compresult)
+    return str(compresult)
+
+#
+# @app.route("/compare", methods=["POST", "GET"])
+# def compare():
+# if request.method == "POST":
+#    simscore = request.form["xy"]
+#    return redirect(url_for("score", simscore=score))
+# else:
+#    return render_template("compare.html")
+# @app.route("/score")
+# def score():
+# def score (simscore):
+# return f"<h1>{simscore}</h1>"
+# return "hello"
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
+
+
+
+
