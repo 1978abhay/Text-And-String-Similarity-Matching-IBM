c4a18cf5e4f6efd38f7634bcd672f10b8a50f312
bfeeney6 <123484436+bfeeney6@users.noreply.github.com>
2023-03-08 20:23:49 +0000

    just added the getaverage file

--- getaverage.py
+++ getaverage.py
@@ -1,15 +1,18 @@
-def bert(a, b):
-    'From https://towardsdatascience.com/bert-for-measuring-text-similarity-eec91c6bf9e1'
-    from sentence_transformers import SentenceTransformer
-   
-    model = SentenceTransformer('paraphrase-MiniLM-L3-v2')
-    sentence_embeddings = model.encode([a, b])
 
-    from sklearn.metrics.pairwise import cosine_similarity
-
-    return cosine_similarity([sentence_embeddings[0]],
-                             sentence_embeddings[1:]).item(0)
-  
+from cosine import get_cosine, text_to_vector
+import mongeElkanTest 
+import mongeElkan as me
+import checkingifsynonym as checkSyn
+from jaccard import jaccard_index
 X = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
 Y = "Bees are flying insects closely related to wasps and ants, known for their role in pollination and, in the case of the best-known bee species, the western honey bee, for producinghoney. Bees are a monophyletic lineage within the superfamily Apoidea. They are presently considered a clade, called Anthophila. There are over 16,000 known species of bees in seven recognized biological families. Some species – including honey bees, bumblebees, and stingless bees – live socially in colonies while some species – including mason bees, carpenter bees, leafcutter bees, and sweat bees – are solitary."
-print(bert(X,Y))
+vector1 = text_to_vector(X)
+vector2 = text_to_vector(Y)
+cosine = get_cosine(vector1, vector2)
+jaccard =(jaccard_index(X,Y))
+cosineVar = round(cosine,3)
+mongeElkan1 = me.longMongeElkan(1, X, Y, lambda a,b: int(checkSyn.anotherWord(a,b)))
+mongeElkan2 = me.quadSimilarityME(X, Y, lambda a,b: int(checkSyn.anotherWord(a,b)))
+average = jaccard + cosineVar + mongeElkan1 + mongeElkan2
+average = average/4
+print(average)
--- jaccard.py
+++ jaccard.py
@@ -12,7 +12,7 @@ def jaccard_index(a: str, b: str) -> float:
     if a is None or b is None:
         raise ValueError("Recieved None argument to jaccard index.")
 
-    def shingle(s: str, w=2) -> List[str]:
+    def shingle(s: str, w=1) -> List[str]:
         """Return the w-shingle of the string s.
 
         :param str s: The string to shingle
@@ -28,3 +28,5 @@ def jaccard_index(a: str, b: str) -> float:
     a_words = set(shingle(a))
     b_words = set(shingle(b))
     return len(a_words.intersection(b_words)) / len(a_words.union(b_words))
+X = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible."
+Y = "Bees are flying insects closely related to wasps and ants, known for their role in pollination and, in the case of the best-known bee species, the western honey bee, for producinghoney. Bees are a monophyletic lineage within the superfamily Apoidea. They are presently considered a clade, called Anthophila. There are over 16,000 known species of bees in seven recognized biological families. Some species – including honey bees, bumblebees, and stingless bees – live socially in colonies while some species – including mason bees, carpenter bees, leafcutter bees, and sweat bees – are solitary."
