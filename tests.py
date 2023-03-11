8bf3c3b4641623d5afb0a8e0ad038e8d84227ed5
JDABH <72359041+JDABH@users.noreply.github.com>
2023-03-11 22:42:48 +0000

    Removed imported file declaration
    Removed the imported file function declaration (e.g. using cosine.text_to_vector() instead of just text_to_vector())

--- tests.py
+++ tests.py
@@ -3,8 +3,8 @@ from cosine import *
 
 # Testing the cosine similarity matching algorithm against two identical strings
 def test_cosine():
-    X = Y = cosine.text_to_vector("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.")
-    assert round(cosine.get_cosine(X,Y), 3)==1.0
+    X = Y = text_to_vector("According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.")
+    assert round(get_cosine(X,Y), 3)==1.0
     
     
 def test_placeholder():
