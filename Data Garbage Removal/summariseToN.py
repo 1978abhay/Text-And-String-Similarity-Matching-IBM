edf81d1333226b2e71016ae03b506a3f2303b31f
ReRi1600 <106563407+ReRi16000@users.noreply.github.com>
2023-03-21 15:51:18 +0000

    scale summary returns more info
    Now returns more info related to the sizes of the inputs. go() function now tests scaled summary.

--- Data Garbage Removal/summariseToN.py
+++ Data Garbage Removal/summariseToN.py
@@ -10,31 +10,43 @@ from collections import defaultdict
 import heapq
 import re
 
-SIZE_REDUCTION = 0.8
+SIZE_REDUCTION = 0.5
 
 # If the two texts are dramatically different sizes, summarises both to within SIZE_REDUCTION many sentences of the smaller text
+# returns the following array:
+# [0] - the first input text, summarised
+# [1] - the second input text, summarised
+# [2] - the sentence count of the first text pre-summary
+# [3] - the sentence count of the second text pre-summary
+# [4] - the new sentence count of the first summarised text
+# [5] - the new sentence count of the second summarised text
 def scale_summary(t1, t2):
 
     t1 = clean(t1)
     t2 = clean(t2)
     sen1 = sent_tokenize(t1)
     sen2 = sent_tokenize(t2)
+    sen1_size = len(sen1)
+    sen2_size = len(sen2)
     word1 = word_tokenize(t1)
     word2 = word_tokenize(t2)
 
     if ( len(word1) / len(word2) >= 1.3 ): # if text 1 has 1.3 or more times as many words as text 2, we want to narrow them down to be similar sizes
-        desired_size = len(sen2) * SIZE_REDUCTION
+        desired_size = sen2_size * SIZE_REDUCTION
         desired_size = round(desired_size)
         t1 = summarise(t1, desired_size)
         t2 = summarise(t2, desired_size)
 
     elif ( len(word2) / len(word1) >= 1.3 ): # if text 2 has 1.3 or more times as many words as text 1, we want to narrow them down to be similar sizes
-        desired_size = len(sen1) * SIZE_REDUCTION
+        desired_size = sen1_size * SIZE_REDUCTION
         desired_size = round(desired_size)
         t1 = summarise(t1, desired_size)
         t2 = summarise(t2, desired_size)
 
-    inputs = [t1, t2]
+    new1_size = len(sent_tokenize(t1))
+    new2_size = len(sent_tokenize(t2))
+
+    inputs = [t1, t2, sen1_size, sen2_size, new1_size, new2_size]
     return inputs
     
 
@@ -92,12 +104,22 @@ def clean(text):
 
 # temporary function to test functionality
 def go():
-    text = """
-    The International Monetary Fund (IMF) has raised its forecast for global growth this year, from 4. 2% to 4. 6%. It said the world economy grew strongly in the first part of this year, mainly thanks to robust growth in Asia. However, the UK was almost unique in having its 2010 growth forecast revised slightly down, while its 2011 forecast was cut by the IMF from 2. 5% to 2. 1%. The IMF also warned risks had increased and there had been a setback in progress towards financial stability. Developed economies have maintained a modest, but steady recovery in the year to date, the supranational agency said. Concerns over the sustainability of government finances in the developed world, especially Greece and others in Europe, were the major threat to global recovery the IMF argued. It said governments should focus on improving their finances, but warned them not to make cutbacks too rapidly. In recent weeks, a number of governments have introduced austerity measures to cut deficit levels.\"Further credible and decisive policy action us needed to resume progress on financial stability and keep the economic recovery on track,\" said Jose Vinals, director of the Fund's monetary and capital markets division. The IMF said that European banks in particular were being affected by the concerns about government debt and so were less wiling to lend to each other. Less credit available to the wider economy could undermine the recovery, it argued. Although contagion to other regions of the world was likely to be limited, there was a risk that Europe's troubles could have a more substantial impact on global economic growth, it said. The IMF significantly raised its 2010 growth forecast for Brazil - to 7. 1% from 5. 5% - as well as for East Asian tiger economies such as South Korea and Taiwan. But the UK's was downgraded to 1. 2% for this year from an earlier forecast of 1. 3%. This made it one of very few countries to have its growth forecast for 2010 downgraded in the report - the IMF's first forecast since publication of George Osborne's emergency budget.\"The IMF doesn't spell out why it is now more gloomy about the UK,\" said the BBC's economics editor Stephanie Flanders.\"But I am assured that last month's Budget is the reason.\"The agency also slightly reduced its 2010 forecast for France, as well as its 2011 forecasts for the Eurozone, Japan, China and Canada. Meanwhile, in a separate report, the IMF called on the US to do more to tackle its budget deficit.\"We see the need for a more ambitious adjustment to stabilise debt than that envisioned by the authorities,\" the Fund said in its mission report, released on Thursday. The report said that the US should target a surplus of 0. 75% of GDP by 2015, compared with a projected deficit of 11% this year - a Herculean task. The agency suggested a number of tax-raising options:cuts in tax credits, particularly for mortgage interest paymentshigher taxes on energya national consumption tax (like VAT)a financial activities taxIt also called on Washington to do more to deal with the projected growing deficit in the social security budget. Among the various negatives for the US economy, the IMF identified:continuing weak consumer spendinglong-term unemploymenta possible double dip in the housing marketthe failure of hundreds of small banks due to losses in commercial real estatetight financing conditions, especially for smaller firmspossible spillover from the European debt crisisThe report was full of praise for the Federal Reserve, saying the US monetary authority ha.\"deftly managed the trade-off between near-term support and medium-term credibility
+    text1 = """
+    Milosevic is running for the 28 December polls as the head of the Socialist Party of Serbia from his prison cell at The Hague.\nThree other suspected war criminals are also contesting the elections. \nMilosevic\u2019s candidacy has alarmed European diplomats and Serbian democrats who helped oust him from power three years ago.\nIn a stark warning, European Union foreign policy envoy Javier Solana said that Brussels expected Serbia to \u201cmake a choice between the past and the future\u201d, as the republic tries to clinch EU membership.\nElection posters of Milosevic in Belgrade have been spat on, defaced and torn down.\nBut he retains his popularity among hardcore Socialists. Analysts said he could very well be elected to parliament.\nMilosevic was dispatched to The Hague in 2001 to face more than 60 charges of war crimes, crimes against humanity and genocide for his role in the wars in Bosnia, Croatia and Kosovo in the 1990s.\nThree other indicted candidates are ultra-nationalist Serbian Radical Party leader Vojislav Seselj, former Yugoslav army chief of staff General Nebojsa Pavkovic and Serbian Deputy Interior Minister Sreten Lukic.\nDemocrats unruffled\nHowever, democratic leaders say that the indicted candidates pose no serious political threat.\n\n\n\n\n\n\u201cSerbia is on its way to Europe. The only question is how fast we will proceed. There is no way to return to isolation\u201d\nMiroljub LabusG17 Plus party leader\n\n\n\n\n\u201cIt will make no impact on the domestic political scene,\u201d Miroljub Labus of the liberal G17 Plus party said.\n\u201cSerbia is on its way to Europe. The only question is how fast we will proceed. There is no way to return to isolation. \u201d\nPolitical analysts predict that a pro-Europe, democratic coalition of the DSS and G17 will get a majority in the 250-seat assembly and reforms designed to bring Serbia closer to Europe will continue.\nKey issue\nBut a key issue for Serbia\u2019s ambitions of joining the European Union is expected to become even more problematic after the polls, namely cooperation with the UN war crimes tribunal at The Hague.\nCentrist parties are against the tribunal, which they blame for fuelling nationalist sentiment with fresh indictments against Serbian generals in advance of the elections.\nLeading moderate candidate, former president Vojislav Kostunica of the Democratic Party of Serbia (DSS), said The Hague was \u201cpoking fun at justice\u201d and repeated his call for a new Serbian law governing Belgrade\u2019s cooperation with the court.\nHis potential coalition partner, Labus, said he was \u201cstrongly in favour\u201d of future war crimes cases being dealt with in Serbian courts.\n"""
+
+    text2 = """
+    The women in the village of Parambu in the southern state of Tamil Nadu have formed a group called Chaaya (Shadow) and acquired land to build the mosque, reported the Hindustan Times daily on Thursday.\u00a0 \u00a0\u00a0 \u201cThis decision was taken after we found male-dominated jamaats (dispute settlement forums) handing down discriminatory verdicts in family disputes, especially in divorce matters,\u201d said Sharifa, the convenor of Chaaya, who uses just one name. \u00a0\u00a0 \u201cWhen a man seeks divorce, only his case is heard by the jamaat. The wife is never called for a hearing, saying that women are not permitted inside mosques, where the jamaat usually sits,\u201d she said. \u00a0\u00a0 A woman\u00a0well-versed in the Quran and Islamic tenets is to take charge at the new mosque.\u00a0\u00a0\u00a0 Every right\n\u201cA survey conducted by us showed that in one out of every five Muslim households, there is at least one case of desertion by the husband or second marriage by the man, citing some mental or physical disability of the first wife,\u201d said Rashida, another villager. \u00a0\u00a0 \u201cAnd when these matters were taken to the police station, they asked us to settle (them) with the jamaat, which are controlled by men,\u201d she said. \u00a0\u00a0 Opinion is divided on whether the women are right to build their own mosque, reported the newspaper. \u00a0\u00a0 Muhammad Sikander, secretary of a jamaat in Tamil Nadu state\u2019s capital Madras, said: \u201cJamaats have been asked to settle disputes outside mosques so that they can hear the women\u2019s side. \u201d \u00a0\u00a0 All India Muslim Personnel Law Board vice-chairman Maulana Kalbe Sadiq said the women had every right to construct a mosque.\n
     """
 
-    summary = summarise(text, 1)
-    print(summary)
+    summary = scale_summary(text1, text2)
+    print("text 1:\n" + summary[0] + "\noriginal sentences: ")
+    print(summary[2])
+    print("current sentences: ")
+    print(summary[4])
+    print("\n\ntext 2:\n" + summary[1] + "\noriginal sentences: ")
+    print(summary[3])
+    print("current sentences: ")
+    print(summary[5])
 
 # temporary function to display all the stopwords in use
 def show_stopwords():
